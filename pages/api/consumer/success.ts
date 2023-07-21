afc454094810237791c355c1b4dee1692e1e06ca
dehu <dehu@ebay.com>
2023-07-21 14:56:51 +0800

    add consume body

--- pages/api/consumer/success.ts
+++ pages/api/consumer/success.ts
@@ -4,6 +4,6 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<void>
 ) {
-  console.log("Get success request");
+  console.log("Get success request" + req.body);
   res.status(200).send();
 }
